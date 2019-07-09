60810d8e7313e81c02b1bb8762fabaa9f7093115
Eugene Lu <qenteen@gmail.com>
2019-07-09 18:03:41 +0300

    + Добавлен EF и простой код добавления записи в БД

--- EFPractice/EF/Program.cs
+++ EFPractice/EF/Program.cs
@@ -10,6 +10,19 @@ namespace EF
     {
         static void Main(string[] args)
         {
+            var db = new HotelEntities();
+            var guest = new guest()
+            {
+                id = 2,
+                name = "Алёшка",
+                sex = "M",
+                birth = new DateTime(1982, 4, 10),
+                country = "Россия",
+                address = "Не дом и не улица"
+            };
+
+            db.guest.Add(guest);
+            db.SaveChanges();
         }
     }
 }
